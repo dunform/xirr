4de1fc34e8b3616fc31a51be80d63c16176a8c7d
dunform <mike.dunford@gmail.com>
2012-07-22 18:59:59 +0100

    Created project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xirr::Application.config.secret_token = 'b8b8052c856c1de01d1ef28171514aa38a7d44b02bf8f3948dc629487102c9cd6c7d70f4fc90bdbd72fcf8dbd63f04e08ee2aad7563c4dbee7d81e298b6e2b98'
